44c45cbb406afb17fb250d21b7d994553628ed43
Walter Zimmerman <zimmermanw84@gmail.com>
2015-06-12 09:33:48 -0700

    added email to user model and simple auth

--- models/users.js
+++ models/users.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('User', new Schema({
 
   username: { type: String, required: true , unique: true },
+  email: String,
   googleID: Number,
 
 }));
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 // var models = require('../models/users');
 var User = require('../models/users');
 /* GET home page. */
+
+// Simple auth
+var authUser = function(req, res, next) {
+  if (!req.session.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
 router.get('/', function(req, res, next) {
   // console.log("User MODEL", models);
   res.render('index', { title: 'Express' });
 });
 
-router.get('/success', function(req, res, next) {
+router.get('/success', authUser, function(req, res, next) {
   res.render('success');
 });
 
